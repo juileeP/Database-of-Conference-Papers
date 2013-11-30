90ab768266851f24909e117c32fa793016925df7
Juilee Patankar <patankarjuilee@gmail.com>
2013-11-29 20:08:02 -0500

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,238 @@
+use jpata;
+delete from Conference_Paper;
+insert into Conference_Paper values 
+('Text Mining in genomics and System Biology','Biomedical informatics','DTMBIO Data and Text Mining in Biomedical Informatics',null,
+ '2008-10-26',3001,'Alfonso Valencia','Napa Valley, CA, USA',0);
+insert into Conference_Paper values
+('Biological Pathways as features for microarray data classification','Biomedical informatics','DTMBIO Data and Text Mining in Biomedical Informatics',null,
+ '2008-10-26',3002,'Brian Quanz','Napa Valley, CA, USA',0);
+insert into Conference_Paper values
+('Identification of temporal association rules from time-series microarray data set','Biomedical informatics','DTMBIO Data and Text Mining in Biomedical Informatics',null,
+ '2008-10-26',3003,'Hojung Nam','Napa Valley, CA, USA',0);
+insert into Conference_Paper values
+('Text mining for Pharmacogenomics','Biomedical informatics','DTMBIO Data and Text Mining in Biomedical Informatics',null,
+ '2008-10-26',3004,'Russ B. Altman','Napa Valley, CA, USA',0);
+ insert into Conference_Paper values
+('Microarray data analysis with PCA in DBMS','Biomedical informatics','DTMBIO Data and Text Mining in Biomedical Informatics',null,
+ '2008-10-26',3005,'Carlos Ordonez','Napa Valley, CA, USA',0);
+
+ insert into Conference_Paper values 
+('ArtSurf: A Method for Deformable partial matching of protein small-molecule binding sites','Bioinformatics, Computational Biology',
+ 'BCB Bioinformatics, Computational Biology and Biomedicine',null,'2012-10-08',3006,'Jeffrey Voorst','Orlando, FL, USA',0);
+ insert into Conference_Paper values 
+('ERENE-BS5: aligning BS-treated sequences by multiple hits on a 5-letters alphabet','Bioinformatics, Computational Biology',
+ 'BCB Bioinformatics, Computational Biology and Biomedicine',null,'2012-10-08',3007,'Nicola Prezza','Orlando, FL, USA',0);
+ insert into Conference_Paper values 
+('Improved multiple sequence alignments using coupled pattern mining','Bioinformatics, Computational Biology',
+ 'BCB Bioinformatics, Computational Biology and Biomedicine',null,'2012-10-08',3008,'Tozammel Hossain','Orlando, FL, USA',0);
+ insert into Conference_Paper values 
+('NetPipe: A network-based pipeline for discovery of genes and protein complexes regulating meiotic recombination hotspots','Bioinformatics, Computational Biology',
+ 'BCB Bioinformatics, Computational Biology and Biomedicine',null,'2012-10-08',3009,'Min Wu','Orlando, FL, USA',0);
+ insert into Conference_Paper values 
+('STEROID: In silico heuristic target combination identification for disease-related signaling networks','Bioinformatics, Computational Biology',
+ 'BCB Bioinformatics, Computational Biology and Biomedicine',null,'2012-10-08',3010,'Huey Eng Chua','Orlando, FL, USA',0);
+
+ insert into Conference_Paper values 
+('A uniform parallel optimization method for data mining grid','Artificial intelligence','AIGC Artificial Intelligence in Grid Computing',
+ null,'2007-08-14',3011,'Kun Gao','New York, NY, USA',0);
+  insert into Conference_Paper values 
+('A novel image edge detection using fractal compression','Artificial intelligence','AIGC Artificial Intelligence in Grid Computing',
+ null,'2007-08-14',3012,'Liangbin Zang','New York, NY, USA',0);
+  insert into Conference_Paper values 
+('Research on new high accuracy controller on periodic motion','Artificial intelligence','AIGC Artificial Intelligence in Grid Computing',
+ null,'2007-08-14',3013,'Ke Zhang','New York, NY, USA',0);
+  insert into Conference_Paper values 
+('Research on webGIS based on multi-agent','Artificial intelligence','AIGC Artificial Intelligence in Grid Computing',
+ null,'2007-08-14',3014,'Taowei Wang','New York, NY, USA',0);
+  insert into Conference_Paper values 
+('Study of intrusion detection system based on improved BP neural networks','Artificial intelligence','AIGC Artificial Intelligence in Grid Computing',
+ null,'2007-08-14',3015,'Kun Gao','New York, NY, USA',0);
+
+insert into Conference_Paper values 
+('An abstract machine based on classical association psychology','Electronic electrical','AIE Spring joint computer conference',
+ null,'1962-05-01',3016,'Richard Reiss','New York, NY, USA',0);
+ insert into Conference_Paper values 
+('A nonlinear digital optimizing program for process control systems','Electronic electrical','AIE Spring joint computer conference',
+ null,'1962-05-01',3017,'Raymond Mugele','New York, NY, USA',0);
+ insert into Conference_Paper values 
+('A simulation of a business firm','Electronic electrical','AIE Spring joint computer conference',
+ null,'1962-05-01',3018,'Charles Bonini','New York, NY, USA',0);
+ insert into Conference_Paper values 
+('A superconductive associative memory','Electronic electrical','AIE Spring joint computer conference',
+ null,'1962-05-01',3019,'Paul Davies','New York, NY, USA',0);
+ insert into Conference_Paper values 
+('Toward a general simulation capability','Electronic electrical','AIE Spring joint computer conference',
+ null,'1962-05-01',3020,'Michael Lackner','New York, NY, USA',0);
+
+insert into Conference_Paper values 
+('2D similarity kernels for biological sequence classification','Data mining bioinformatics','BIOKDD Data mining in bioinformatics',
+ null,'2012-08-12',3021,'Pavel Kuksa','Beijing, China',0);
+ insert into Conference_Paper values 
+('Biomedical text categorization with concept graph representations using a controlled vocabulary','Data mining bioinformatics','BIOKDD Data mining in bioinformatics',
+ null,'2012-08-12',3022,'Meenakshi Mishra','Beijing, China',0);
+ insert into Conference_Paper values 
+('Detecting protein complexes from noisy protein interaction data','Data mining bioinformatics','BIOKDD Data mining in bioinformatics',
+ null,'2012-08-12',3023,'Nazar Zaki','Beijing, China',0);
+ insert into Conference_Paper values 
+('Globalised bipartite local model for drug-target interaction prediction','Data mining bioinformatics','BIOKDD Data mining in bioinformatics',
+ null,'2012-08-12',3024,'Peng Yang','Beijing, China',0);
+ insert into Conference_Paper values 
+('Learning to extract chemical names based on random text generation and incomplete dictionary','Data mining bioinformatics','BIOKDD Data mining in bioinformatics',
+ null,'2012-08-12',3025,'Su Yan','Beijing, China',0);
+
+ delete from Author;
+ insert into Author values
+ (3001,'Alfonso Valencia','Spanish National Cancer Research centre','valencia@cnio.es');
+ insert into Author values
+ (3002,'Brian Quanz','University of Kansas','bquanz@ku.edu');
+ insert into Author values
+ (3003,'Hojung Nam','KAIST','hjanm@kaist.ac.kr');
+ insert into Author values
+ (3004,'Russ B. Altman','Stanford University','russ.altman@stanford.edu');
+ insert into Author values
+ (3005,'Carlos Ordonez','University of Houston','ordonez@cs.uh.edu');
+
+insert into Author values
+(3006,'Jeffrey Voorst','University of Minnesota-Twin cities','vanv0059@umn.edu');
+insert into Author values
+(3007,'Nicola Prezza','University of Udine','prezza.nicola@spes.uniud.it');
+insert into Author values
+(3008,'Tozammel Hossain','Virginia Poly Tech','tozammel@vt.edu');
+insert into Author values
+(3009,'Min Wu','Nanyang Tech University','wumin@ntu.edu.sg');
+insert into Author values
+(3010,'Huey Eng Chua','Nanyang Tech University',null);
+
+insert into Author values
+(3011,'Kun Gao','Zhejiang Wanli University','gaoyibo@gmail.com');
+insert into Author values
+(3012,'Liangbin Zang','Zhejiang Wanli University','zlb@zwu.edu.cn');
+insert into Author values
+(3013,'Ke Zhang','Shanghai Institute of Technology','zhangk2007@gmail.com');
+insert into Author values
+(3014,'Taowei Wang','Zhejiang Wanli University','nbwtw@zwu.edu.cn');
+insert into Author values
+(3015,'Kun Gao','Zhejiang Wanli University','gaoyibo@gmail.com');
+
+insert into Author values
+(3016,'Richard Reiss','General Precision Inc.',null);
+insert into Author values
+(3017,'Raymond Mugele','International Business Machines Corp.',null);
+insert into Author values
+(3018,'Charles Bonini','Stanford University',null);
+insert into Author values
+(3019,'Paul Davies','Abacus Inc.',null);
+insert into Author values
+(3020,'Michael Lackner','System Development Corporation',null);
+
+insert into Author values
+(3021,'Pavel Kuksa','NEC Laboratories Inc.','pkuksa@nec-labs.com');
+insert into Author values
+(3022,'Meenakshi Mishra','University of Kansas','mmishra@ittc.ku.edu');
+insert into Author values
+(3023,'Nazar Zaki','UAEU','nzaki@uaeu.ac.ae');
+insert into Author values
+(3024,'Peng Yang','Nanyang technological University','yang0293@e.ntu.edu.sg');
+insert into Author values
+(3025,'Su Yan','IBM Research Lab','syan@us.ibm.com');
+
+delete from Member;
+insert into Member values
+(1001,'Juilee Patankar','University of Delaware','juilee@udel.edu',null);
+insert into Member values
+(1002,'Prasad Gajare','University of Delaware','prasadg@udel.edu',null);
+insert into Member values
+(1003,'Shreya Maddula','TCS','shreya1710@gmail.com',null);
+insert into Member values
+(1004,'Sushant Patil','University of North Carolina-Charlotte','sushant_dt@yahoo.co.in',null);
+insert into Member values
+(1005,'Devendra Desai','Nomura Services','devendra2206@gmail.com',null);
+
+delete from Administrator;
+insert into Administrator values
+(5001,'Juilee Patankar','juilee@udel.edu');
+insert into Administrator values
+(5000,'Prasad Gajare','prasadg@udel.edu');
+
+delete from citedBy;
+insert into citedBy values
+(1001,null);
+insert into citedBy values
+(1002,null);
+insert into citedBy values
+(1003,null);
+insert into citedBy values
+(1004,null);
+insert into citedBy values
+(1005,null);
+
+delete from accessedBy;
+insert into accessedBy values
+(1001,null);
+insert into accessedBy values
+(1002,null);
+insert into accessedBy values
+(1003,null);
+insert into accessedBy values
+(1004,null);
+insert into accessedBy values
+(1005,null);
+
+delete from publishedBy;
+insert into publishedBy values
+(3001,'Text Mining in genomics and System Biology');
+insert into publishedBy values
+(3002,'Biological Pathways as features for microarray data classification');
+insert into publishedBy values
+(3003,'Identification of temporal association rules from time-series microarray data set');
+insert into publishedBy values
+(3004,'Text mining for Pharmacogenomics');
+insert into publishedBy values
+(3005,'Microarray data analysis with PCA in DBMS');
+insert into publishedBy values
+(3006,'ArtSurf: A Method for Deformable partial matching of protein small-molecule binding sites');
+insert into publishedBy values
+(3007,'ERENE-BS5: aligning BS-treated sequences by multiple hits on a 5-letters alphabet');
+insert into publishedBy values
+(3008,'Improved multiple sequence alignments using coupled pattern mining');
+insert into publishedBy values
+(3009,'NetPipe: A network-based pipeline for discovery of genes and protein complexes regulating meiotic recombination hotspots');
+insert into publishedBy values
+(3010,'STEROID: In silico heuristic target combination identification for disease-related signaling networks');
+insert into publishedBy values
+(3011,'A uniform parallel optimization method for data mining grid');
+insert into publishedBy values
+(3012,'A novel image edge detection using fractal compression');
+insert into publishedBy values
+(3013,'Research on new high accuracy controller on periodic motion');
+insert into publishedBy values
+(3014,'Research on webGIS based on multi-agent');
+insert into publishedBy values
+(3015,'Study of intrusion detection system based on improved BP neural networks');
+insert into publishedBy values
+(3016,'An abstract machine based on classical association psychology');
+insert into publishedBy values
+(3017,'A nonlinear digital optimizing program for process control systems');
+insert into publishedBy values
+(3018,'A simulation of a business firm');
+insert into publishedBy values
+(3019,'A superconductive associative memory');
+insert into publishedBy values
+(3020,'Toward a general simulation capability');
+insert into publishedBy values
+(3021,'2D similarity kernels for biological sequence classification');
+insert into publishedBy values
+(3022,'Biomedical text categorization with concept graph representations using a controlled vocabulary');
+insert into publishedBy values
+(3023,'Detecting protein complexes from noisy protein interaction data');
+insert into publishedBy values
+(3024,'Globalised bipartite local model for drug-target interaction prediction');
+insert into publishedBy values
+(3025,'Learning to extract chemical names based on random text generation and incomplete dictionary');
+
+
+
+
+
+
+
